2c2575b6e2d3e7c4ab0d89a97bcc7d172c0d2d68
tisonpatrik <patriktison@gmail.com>
2024-06-14 15:27:58 +0200

    copilot was simplified

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,11 +2,5 @@ local plugins = {
   {
     "github/copilot.vim",
     lazy = false,
-    config = function()
-      vim.g.copilot_no_tab_map = true
-      vim.g.copilot_assume_mapped = true
-      vim.g.copilot_tab_fallback = ""
-    end,
-  },
-}
+  },}
 return plugins
